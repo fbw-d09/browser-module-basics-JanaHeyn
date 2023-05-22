9afb4135cbfe268e2dc23d63e8f8830e848b0632
JanaHeyn <jana.heyn@web.de>
2023-05-22 11:50:39 +0200

    Update tasks

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,6 @@
-import { clickImages } from "./click";
-import { hoverHeader } from "./hover";
-import { scrollWindow } from "./scroll";
+import { clickImages } from "./click.js";
+import { hoverHeader } from "./hover.js";
+import { scrollWindow } from "./scroll.js";
 
 hoverHeader();
 clickImages();
